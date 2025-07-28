f5b37c910fb2f9caee43fd9111eec2f86e9a6094
ElG87 <lil_gomes87@hotmail.com>
2025-07-29 00:32:59 +0200

    functie overMijnFoto aangeroepen

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const mijnFoto = document.getElementById("mijn-foto-home");
 mijnFoto.addEventListener("click", function() {
     window.location.href = "../html/overmij.html";
 }
-)}
+)};
+overMijFoto();
 
 
 function huidigeLeeftijd() {
