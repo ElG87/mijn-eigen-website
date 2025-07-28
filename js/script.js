25842c29cc66d73fc37101fe1869bc703614e3c2
ElG87 <lil_gomes87@hotmail.com>
2025-07-29 00:18:38 +0200

    functie aangemaakt met js om leeftijd automatisch aan te laten passen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+function overMijFoto() {
 const mijnFoto = document.getElementById("mijn-foto-home");
 
 mijnFoto.addEventListener("click", function() {
     window.location.href = "../html/overmij.html";
 }
-);
+)};
+
+function huidigeLeftijd() {
+    const geboorte = new Date("1987-04-24");
+    const vandaag = new Date();
+    let leeftijd = vandaag.getFullYear() - geboorte.getFullYear();
+    const verschilMaand = vandaag.getMonth() - geboorte.getMonth();
+
+    if (verschilMaand < 0 || (verschilMaand === 0 && vandaag.getDate() < geboorte.getDate())) {
+        leeftijd--;
+    }
+
+    document.getElementById("leeftijd").textContent = leeftijd;
+}
+;
+huidigeLeftijd();
